3ac9d101d4823cea031a708b91db9c8040e0d62c
zvxr <zvxr.zppbaaryy@gmail.com>
2016-11-22 11:09:06 -0800

    work on blowfish

--- crypto/classes/ciphers/blowfish.py
+++ crypto/classes/ciphers/blowfish.py
@@ -23,64 +23,58 @@ class BlowfishCipher(BlockCipher):
 
     @BlockCipher.key.setter
     def key(self, value):
-        if value is not None and len(value) not in (16, 24, 32):
+        if value is not None and not (4 <= len(value) <= 56):
             raise AttributeError(
-                "key must be 16 (AES-128), 24 (AES-192), or 32 (AES-256) bytes long."
+                "key must be between 4 and 56 bytes long."
             )
         self._key = value
 
     @BlockCipher.iv.setter
     def iv(self, value):
         # Ignore IV for ECB mode.
-        if self._mode == AES.MODE_ECB:
+        if self._mode == Blowfish.MODE_ECB:
             return
 
-        if value is not None and len(value) != AES.block_size:
+        if value is not None and len(value) != Blowfish.block_size:
             raise AttributeError(
-                "iv must be {} bytes long.".format(AES.block_size)
+                "iv must be {} bytes long.".format(Blowfish.block_size)
             )
         self._iv = value
 
-    def _get_counter(self):
-        """Returns a stateful Counter instance of 128 bits to work
-        with AES key sizes. No prefix or suffix is applied.
-        """
-        return Counter(128, initial_value=self.initial_value)
-
     def _get_cipher(self):
-        """Return a Pycrypto AES cipher instance.
+        """Return a Pycrypto Blowfish cipher instance.
         `key`, `mode` and depending on mode `iv` must be set.
         """
-        if self._mode == AES.MODE_ECB:
-            return AES.new(self.key, self._mode)
-        elif self._mode == AES.MODE_CTR:
-            return AES.new(self.key, self._mode, counter=self._get_counter())
+        if self._mode == Blowfish.MODE_ECB:
+            return Blowfish.new(self.key, self._mode)
+        elif self._mode == Blowfish.MODE_CTR:
+            return Blowfish.new(self.key, self._mode, counter=self._get_counter())
         else:
-            return AES.new(self.key, self._mode, self.iv)
+            return Blowfish.new(self.key, self._mode, self.iv)
 
     def encrypt(self, plaintext):
         """Generate cipher, encrypt, and encode data."""
-        aes_cipher = self._get_cipher()
-        padded_plaintext = self.pad(plaintext, AES.block_size)
-        ciphertext = aes_cipher.encrypt(padded_plaintext)
+        blowfish_cipher = self._get_cipher()
+        padded_plaintext = self.pad(plaintext, Blowfish.block_size)
+        ciphertext = blowfish_cipher.encrypt(padded_plaintext)
         return self._encode(ciphertext)
 
     def decrypt(self, ciphertext):
         """Generate cipher, decode, and decrypt data."""
-        aes_cipher = self._get_cipher()
+        blowfish_cipher = self._get_cipher()
         decoded_ciphertext = self._decode(ciphertext)
-        plaintext = aes_cipher.decrypt(decoded_ciphertext)
+        plaintext = blowfish_cipher.decrypt(decoded_ciphertext)
         return self.unpad(plaintext)
 
     def generate_iv(self):
-        """Randomly generate an IV byte string for various modes of AES."""
-        return Random.new().read(AES.block_size)
+        """Randomly generate an IV byte string for various modes of Blowfish."""
+        return Random.new().read(Blowfish.block_size)
 
-    def generate_key(self, key_size=16):
-        """Randomly generate a key of byte size `key_size`. Must be 16, 24, or 32."""
-        if key_size not in (16, 24, 32):
+    def generate_key(self, key_size=56):
+        """Randomly generate a key of byte size `key_size`. Must be between 4 and 56."""
+        if not 4 <= key_size <= 56:
             raise AttributeError(
-                "key_size must be 16 (AES-128), 24 (AES-192), or 32 (AES-256)."
+                "key_size must be between 4 and 56 bytes."
             )
         random_device = Random.new()
         return random_device.read(key_size)
