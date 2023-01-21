3ee4a0959aff56dbc2b570a967287a33687168d8
zvxr <zvxr.zppbaaryy@gmail.com>
2023-01-21 13:07:58 -0800

    hack for py3

--- crypto/classes/ciphers/xor.py
+++ crypto/classes/ciphers/xor.py
@@ -3,8 +3,9 @@ import string
 
 from crypto.classes.ciphers.base import CryptoCipher
 #from crypto.ports.xor import XOR
+from Crypto.Cipher import XOR
 from Crypto.Util import strxor
-from mock import Mock; XOR = Mock()
+#from mock import Mock; XOR = Mock()
 from Crypto import Random
 
 
@@ -17,33 +18,35 @@ class XORCipher(CryptoCipher):
     def __init__(self, key=None):
         super(XORCipher, self).__init__(key)
 
-    @CryptoCipher.key.setter
-    def key(self, value):
-        #if value is not None and len(value) not in XOR.key_size:
-        #    raise AttributeError(
-        #        "key must be %s - %s bytes long." % (
-        #            XOR.key_size[0],
-        #            XOR.key_size[-1]
-        #        )
-        #    )
-        self._key = bytes(value, 'utf-8')
+    #@CryptoCipher.key.setter
+    #def key(self, value):
+    #    #if value is not None and len(value) not in XOR.key_size:
+    #    #    raise AttributeError(
+    #    #        "key must be %s - %s bytes long." % (
+    #    #            XOR.key_size[0],
+    #    #            XOR.key_size[-1]
+    #    #        )
+    #    #    )
+    #    self._key = value
+    #    # TODO py2/3
+    #    #self._key = bytes(value, 'utf-8')
 
     def encrypt(self, plaintext):
         """Generate cipher, encrypt, and encode data."""
-        #xor_cipher = XOR.new(self.key)
-        #ciphertext = xor_cipher.encrypt(plaintext)
-        ciphertext = strxor.strxor(self.key, bytes(plaintext, 'utf-8'))
+        xor_cipher = XOR.new(self.key)
+        ciphertext = xor_cipher.encrypt(plaintext)
+        #ciphertext = strxor.strxor(self.key, bytes(plaintext, 'utf-8'))
         return self._encode(ciphertext)
 
     def decrypt(self, ciphertext):
         """Generate cipher, decode, and decrypt data."""
-        #xor_cipher = XOR.new(self.key)
-        decoded_ciphertext = self._decode(ciphertext)
-        #return xor_cipher.decrypt(decoded_ciphertext)
-        key = self.key * (len(decoded_ciphertext) // len(self.key))
-        if len(decoded_ciphertext) % len(self.key):
-            key += self.key[:len(decoded_ciphertext) % len(self.key)]
-        return strxor.strxor(key, decoded_ciphertext)
+        xor_cipher = XOR.new(self.key)
+        #decoded_ciphertext = self._decode(ciphertext)
+        return xor_cipher.decrypt(decoded_ciphertext)
+        #key = self.key * (len(decoded_ciphertext) // len(self.key))
+        #if len(decoded_ciphertext) % len(self.key):
+        #    key += self.key[:len(decoded_ciphertext) % len(self.key)]
+        #return strxor.strxor(key, decoded_ciphertext)
 
     def generate_key(self, key_size=16, ascii_only=True):
         """Randomly generate a key of byte size `key_size`.
--- crypto/quick.py
+++ crypto/quick.py
@@ -33,33 +33,3 @@ def decrypt():
     # Debugging for now.
     args = parser.parse_args(["cipher","xor","-c"])
     args.execute(args)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        conflict_handler="resolve",
-        prog="pycrypto-cli"
-    )
-    mode_parser = parser.add_subparsers(
-        help="Pycrypto module to use."
-    )
-
-    data_parser = argparse.ArgumentParser(add_help=False)
-    base_cli.add_parser_args(data_parser)
-
-    cipher_parser = mode_parser.add_parser(
-        "cipher",
-        parents=[data_parser],
-        help="Use cipher module."
-    )
-    cipher_cli.add_parser_args(cipher_parser)
-
-    hash_parser = mode_parser.add_parser(
-        "hash",
-        parents=[data_parser],
-        help="Use hash module."
-    )
-
-    # Debugging for now.
-    args = parser.parse_args()
-    args.execute(args)
